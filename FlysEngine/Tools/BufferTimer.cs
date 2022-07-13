a2533ba68aa83f20580cf78f6e745d33a96c2598
sdcb <sdflysha@qq.com>
2022-07-13 16:58:25 +0800

    Add BufferTimer.

---
+++ FlysEngine/Tools/BufferTimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FlysEngine.Tools
+{
+    public class BufferTimer
+	{
+		float _total = 0;
+
+		public void Tick(float dt, float bufferTime, Action action)
+		{
+			_total += dt;
+			while (_total > bufferTime)
+			{
+				action();
+				_total -= bufferTime;
+			}
+		}
+	}
+}
